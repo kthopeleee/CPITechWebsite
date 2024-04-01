a28e4cc676bf5ed78dff125ed05168ce552ab7f8
kthopeleee <khl2145@barnard.edu>
2024-04-01 19:50:06 -0400

    added js and styles to make it look better

---
+++ scripts.js
@@ -0,0 +1,11 @@
+// Add any JavaScript you need here
+// For example, for smooth scrolling when the anchor links are clicked:
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
